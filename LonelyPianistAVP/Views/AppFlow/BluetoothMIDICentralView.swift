49d870555655faa7c4fba61957711d00c5e57a64
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-05-16 13:47:29 +0800

    refactor: 删除BluetoothMIDICentralView未使用的sheet版本及其Coordinator
    BluetoothMIDICentralView（带@Binding isPresented的sheet版本）在项目中无任何引用，
    仅BluetoothMIDICentralEmbeddedView被BluetoothMIDIPreparationView使用。

--- LonelyPianistAVP/Views/AppFlow/BluetoothMIDICentralView.swift
+++ LonelyPianistAVP/Views/AppFlow/BluetoothMIDICentralView.swift
@@ -11,35 +11,3 @@ struct BluetoothMIDICentralEmbeddedView: UIViewControllerRepresentable {
     func updateUIViewController(_: UINavigationController, context _: Context) {}
 }
 
-struct BluetoothMIDICentralView: UIViewControllerRepresentable {
-    @Binding var isPresented: Bool
-
-    func makeCoordinator() -> Coordinator {
-        Coordinator(isPresented: $isPresented)
-    }
-
-    func makeUIViewController(context: Context) -> UINavigationController {
-        let controller = CABTMIDICentralViewController()
-        let navigationController = UINavigationController(rootViewController: controller)
-        navigationController.presentationController?.delegate = context.coordinator
-        return navigationController
-    }
-
-    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
-        uiViewController.presentationController?.delegate = context.coordinator
-    }
-}
-
-extension BluetoothMIDICentralView {
-    final class Coordinator: NSObject, UIAdaptivePresentationControllerDelegate {
-        private let isPresented: Binding<Bool>
-
-        init(isPresented: Binding<Bool>) {
-            self.isPresented = isPresented
-        }
-
-        func presentationControllerDidDismiss(_: UIPresentationController) {
-            isPresented.wrappedValue = false
-        }
-    }
-}
