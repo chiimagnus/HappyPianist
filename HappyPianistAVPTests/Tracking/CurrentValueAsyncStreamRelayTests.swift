e67955efbb8dcaa2affddde3d5baed876f61670f
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-07-14 14:13:43 +0800

    消除 Relay 取消测试的调度竞争

--- HappyPianistAVPTests/Tracking/CurrentValueAsyncStreamRelayTests.swift
+++ HappyPianistAVPTests/Tracking/CurrentValueAsyncStreamRelayTests.swift
@@ -7,15 +7,23 @@ func cancellingAnOlderRelaySubscriberDoesNotDisconnectTheRemainingSubscriber() a
     let relay = CurrentValueAsyncStreamRelay(0)
     let firstStream = relay.makeStream()
     let secondStream = relay.makeStream()
+    let firstConsumerReady = AsyncStream<Void>.makeStream()
 
     let firstConsumer = Task { @MainActor in
+        var isFirstValue = true
         for await _ in firstStream {
             guard Task.isCancelled == false else { return }
+            if isFirstValue {
+                isFirstValue = false
+                firstConsumerReady.continuation.yield()
+            }
         }
     }
     var secondIterator = secondStream.makeAsyncIterator()
+    var firstConsumerReadyIterator = firstConsumerReady.stream.makeAsyncIterator()
 
     #expect(await secondIterator.next() == 0)
+    _ = await firstConsumerReadyIterator.next()
     #expect(relay.activeSubscriberCount == 2)
 
     firstConsumer.cancel()
