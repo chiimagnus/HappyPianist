dd227e5532e11351e0170cd330fff2b9cc870de1
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <151696847+chiimagnus@users.noreply.github.com>
2026-07-15 12:06:21 +0800

    删除练习返回时的未选曲占位视图

--- HappyPianistAVP/Views/Practice/PracticeLaunchContainerView.swift
+++ HappyPianistAVP/Views/Practice/PracticeLaunchContainerView.swift
@@ -9,11 +9,7 @@ struct PracticeLaunchContainerView: View {
         Group {
             switch launchViewModel.state {
             case .noRequest:
-                ContentUnavailableView(
-                    "尚未选择练习曲目",
-                    systemImage: "music.note.list",
-                    description: Text("请返回选曲库并选择一首曲目。")
-                )
+                EmptyView()
             case .requested, .loading:
                 ProgressView("正在准备练习…")
                     .controlSize(.large)
